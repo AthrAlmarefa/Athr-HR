4101cc843ec2ccbea5805cf5c19c4ebd9d8e48f6
Somaya Mahmoud <81088289+somaya-mahmoud@users.noreply.github.com>
2025-11-20 07:14:09 +0200

    add profile view widget to handle length of build and to profile details.

--- lib/features/profile/view/profile_view.dart
+++ lib/features/profile/view/profile_view.dart
@@ -1,11 +1,8 @@
 import 'package:athr_hr/core/localization/lang_keys.dart';
 import 'package:athr_hr/core/styles/colors/my_colors.dart';
-import 'package:athr_hr/core/styles/fonts/my_fonts.dart';
 import 'package:athr_hr/core/utils/extension/my_context.dart';
 import 'package:athr_hr/core/widgets/custom_app_bar_row.dart';
-import 'package:athr_hr/features/profile/widgets/custom_profile_data.dart';
-import 'package:athr_hr/features/profile/widgets/custom_row.dart';
-import 'package:athr_hr/features/profile/widgets/custom_switch_icon.dart';
+import 'package:athr_hr/features/profile/widgets/profile_widget.dart';
 import 'package:athr_hr/generated/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -39,89 +36,7 @@ class _ProfileViewState extends State<ProfileView> {
                 ),
               ),
             ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    CustomProfileData(
-                      imageUrl: Assets.imagesProfile,
-                      name: context.translate(LangKeys.name),
-                      jobDescription: context.translate(LangKeys.job),
-                      id: context.translate(LangKeys.id),
-                      prefix: context.translate(LangKeys.prefix),
-                    ),
-                    CustomRow(
-                      imageIcon: Image.asset(Assets.imagesArrowIcon),
-                      widget: Image.asset(Assets.imagesProfileIcon),
-                      mainText: context.translate(LangKeys.personalData),
-                      text: '',
-                      imageWidth: 24.w,
-                      imageHeight: 24.h,
-                      onPressed: () {
-                        widget.onChangeTab?.call(6);
-                      },
-                    ),
-                    CustomRow(
-                      imageIcon: Image.asset(Assets.imagesArrowIcon),
-                      widget: Image.asset(Assets.imagesLeavesIcon),
-                      text: context.translate(LangKeys.trackLeaveBalance),
-                      mainText: context.translate(LangKeys.leaveBalance),
-                      imageWidth: 24.w,
-                      imageHeight: 24.h,
-                    ),
-                    CustomRow(
-                      imageIcon: CustomSwitch(
-                        value: true,
-                        onChanged: (bool value) {},
-                      ),
-                      widget: Image.asset(Assets.imagesNotifications),
-                      text: context.translate(LangKeys.controlAppNotifications),
-                      mainText: context.translate(LangKeys.notifications),
-                      imageHeight: 34.h,
-                      imageWidth: 52.w,
-                    ),
-                    CustomRow(
-                      imageIcon: Image.asset(Assets.imagesArrowIcon),
-                      widget: Image.asset(Assets.imagesLock),
-                      text: context.translate(LangKeys.changeAccountPassword),
-                      mainText: context.translate(LangKeys.securitySettings),
-                      imageWidth: 24.w,
-                      imageHeight: 24.h,
-                      onPressed: () {
-                        widget.onChangeTab?.call(5);
-                      },
-                    ),
-                    CustomRow(
-                      imageIcon: CustomSwitch(
-                        value: true,
-                        onChanged: (bool value) {},
-                      ),
-                      widget: Image.asset(Assets.imagesExistance),
-                      text: context.translate(
-                        LangKeys.autoAttendanceDescription,
-                      ),
-                      mainText: context.translate(LangKeys.autoAttendance),
-                      imageHeight: 34.h,
-                      imageWidth: 52.w,
-                    ),
-                    CustomRow(
-                      imageIcon: Text(
-                        context.translate(LangKeys.english),
-                        style: MyFonts.styleMedium500_16.copyWith(
-                          color: MyColors.black,
-                        ),
-                      ),
-                      imageWidth: 60.w,
-                      imageHeight: 30.h,
-                      widget: Image.asset(Assets.imagesLanguage),
-                      text: context.translate(LangKeys.changeLanguageToEnglish),
-                      mainText: context.translate(LangKeys.language),
-                    ),
-                    SizedBox(height: 30.h),
-                  ],
-                ),
-              ),
-            ),
+            ProfileWidget(),
           ],
         ),
       ),
