45e8e4be592811be8cf70145ccdd0d660d26d357
Mariem Ramadan <156951540+mariam00001@users.noreply.github.com>
2025-11-30 17:10:10 +0200

    #60
    fix #60 [Service] Notification Service UI

--- Client/Client.Admin/src/app/components/bonus-ui/toast/widgets/message-toast/message-toast.component.ts
+++ Client/Client.Admin/src/app/components/bonus-ui/toast/widgets/message-toast/message-toast.component.ts
@@ -1,33 +1,56 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 import { CardComponent } from "../../../../../shared/components/ui/card/card.component";
-import { FeatherIconComponent } from '../../../../../shared/components/ui/feather-icon/feather-icon.component';
+import { FeatherIconComponent } from "../../../../../shared/components/ui/feather-icon/feather-icon.component";
+import { NotificationService } from "../../../../../shared/services/notification.service";
 
 @Component({
-  selector: 'app-message-toast',
+  selector: "app-message-toast",
   imports: [CardComponent, FeatherIconComponent],
-  templateUrl: './message-toast.component.html',
-  styleUrl: './message-toast.component.scss'
+  templateUrl: "./message-toast.component.html",
+  styleUrl: "./message-toast.component.scss",
 })
-
 export class MessageToastComponent {
-
-  public toast = {
+  toast = {
     success: false,
     warning: false,
-    error: false
+    error: false,
+  };
+
+  constructor(private notificationService: NotificationService) {}
+
+  ngOnInit() {
+    this.notificationService.toastState$.subscribe((type) => {
+      this.showToast(type);
+    });
   }
 
-  showToast(value: keyof typeof this.toast) {
-    this.toast[value] = true;
+  showToast(type: keyof typeof this.toast) {
+    this.toast[type] = true;
 
     setTimeout(() => {
-      this.toast[value] = false;
+      this.toast[type] = false;
     }, 5000);
   }
 
-  closeToast(value: keyof typeof this.toast) {
-    this.toast[value] = false;
+  closeToast(type: keyof typeof this.toast) {
+    this.toast[type] = false;
   }
-  
+  // public toast = {
+  //   success: false,
+  //   warning: false,
+  //   error: false,
+  // };
+
+  // showToast(value: keyof typeof this.toast) {
+  //   this.toast[value] = true;
+
+  //   setTimeout(() => {
+  //     this.toast[value] = false;
+  //   }, 5000);
+  // }
+
+  // closeToast(value: keyof typeof this.toast) {
+  //   this.toast[value] = false;
+  // }
 }
--- Client/Client.Admin/src/app/shared/services/notification.service.ts
+++ Client/Client.Admin/src/app/shared/services/notification.service.ts
@@ -1,15 +1,37 @@
-// import {inject, Injectable} from "@angular/core";
-// import {MessageToastComponent} from "../../components/bonus-ui/toast/widgets/message-toast/message-toast.component";
-//
-// @Injectable({
-//     providedIn: 'root'
-// })
-// export class NotificationService {
-//
-//     messageToastComponent = inject(MessageToastComponent);
-//
-//     showSuccess(message: string): void {
-//         this.messageToastComponent.showToast(message);
-//     }
-//
-// }
+import {inject, Injectable} from "@angular/core";
+import {MessageToastComponent} from "../../components/bonus-ui/toast/widgets/message-toast/message-toast.component";
+import { Subject } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class NotificationService {
+  
+  private toastSubject = new Subject<"success" | "warning" | "error">();
+  toastState$ = this.toastSubject.asObservable();
+
+  showSuccess() {
+    this.toastSubject.next("success");
+  }
+
+  showWarning() {
+    this.toastSubject.next("warning");
+  }
+
+  showError() {
+    this.toastSubject.next("error");
+  }
+
+  //   messageToastComponent = inject(MessageToastComponent);
+
+  //   showSuccess(message : string): void {
+  //     this.messageToastComponent.showToast(message);
+  //   }
+  //   showWarning(): void {
+  //     this.messageToastComponent.showToast("warning");
+  //   }
+
+  //   showError(): void {
+  //     this.messageToastComponent.showToast("error");
+  //   }
+}
