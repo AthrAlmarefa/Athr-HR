906d5538478ccc2edf1e99146a2c07561fb33711
Aya Elzoghby <105361358+Aya-Elzoghby21@users.noreply.github.com>
2025-12-07 18:51:19 +0200

    #36-Create taskwork (#123)
    Co-authored-by: Mohamed Elzoghby <malzghby91@gmail.com>

---
+++ Server/src/Athr.Api/Controllers/TaskWorks/TaskWorkController.cs
@@ -0,0 +1,29 @@
+﻿using Asp.Versioning;
+using Athr.Application.TaskWork.Create_TaskWork;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Athr.Api.Controllers.TaskWorks
+{
+    [ApiController]
+    [ApiVersion(ApiVersions.V1)]
+    [Route("api/v{version:apiVersion}/taskwork")]
+    public class TaskWorkController 
+    {
+        private readonly ISender _sender;
+
+        public TaskWorkController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost]
+        public async Task<Guid> CreateTaskWork([FromBody] CreateTaskWorkRequest request)
+        {
+            CreateTaskWorkCommand command = request;
+            var taskId = await _sender.Send(command);
+            return taskId;
+        }
+    }
+}
