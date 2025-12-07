388f422016a824c43cb75615174d8b7657dd301e
Mohamed Elzoghby <malzghby91@gmail.com>
2025-12-07 19:35:21 +0200

    #35change basics

---
+++ Server/src/Athr.Application/TaskWork/Update TaskWork/UpdateTaskWorkCommand.cs
@@ -0,0 +1,15 @@
+﻿using Athr.Application.Abstractions.Messaging;
+namespace Athr.Application.TaskWork.UpdateTaskWork
+{
+    public sealed record UpdateTaskWorkCommand(
+
+        Guid TaskId,
+        string Name,
+        Guid UserId,
+        int PriorityKey,
+        DateTime StartDate,
+        DateTime EndDate,
+        string Description
+    ) : ICommand<Guid>;
+
+}
